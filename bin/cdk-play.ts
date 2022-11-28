97d178e230a0628fff36b62ab57d237ae3f22970
Simon <simon@myhat.net>
2022-11-28 21:17:24 +1300

    security group, start iam role

--- bin/cdk-play.ts
+++ bin/cdk-play.ts
@@ -3,19 +3,11 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkPlayStack } from '../lib/cdk-play-stack';
 
+const envCoreDev  = { account: '136250605156', region: 'ap-southeast-2' };
+const envISOPS = { account: '123abc', region: 'ap-southeast-1' };
+
 const app = new cdk.App();
 new CdkPlayStack(app, 'CdkPlayStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+    env: envCoreDev
 });
+
--- lib/cdk-play-stack.ts
+++ lib/cdk-play-stack.ts
@@ -1,16 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as iam from "aws-cdk-lib/aws-iam";
+
+const vpcName = "CommonVpc"
+const vpcId = "vpc-010eda7a8d6b23ef4"
 
 export class CdkPlayStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = ec2.Vpc.fromLookup(this, vpcName, { vpcId: vpcId })
+    const secGrp = newSecurityGroup(this, vpc)
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPlayQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
+}
+
+function newSecurityGroup(con: Construct, vpc: ec2.IVpc) {
+  const testSG = new ec2.SecurityGroup(con, "testSG", {
+    vpc: vpc,
+    securityGroupName: "TestSecurityGroup",
+    description: "Security Group built by CDK",
+  })
+
+  testSG.addIngressRule(
+      ec2.Peer.ipv4("0.0.0.0/0"),
+      ec2.Port.tcp(8080),
+      "add port 8080")
+  testSG.addIngressRule(
+      ec2.Peer.ipv4("151.210.0.0/16"),
+      ec2.Port.tcp(80),
+      "add port 80 to a restricted range")
+
+  return testSG
+}
+
+function newIamRole(con: Construct) {
+
+  const newRole = new iam.Role(con, "", {assumedBy: undefined})
+
+  return newRole
 }
